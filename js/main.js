268c97a6daaba53ec59ae089f84d032253e57ac0
Keith3895 <keith30895@gmail.com>
2016-05-31 23:01:04 +0530

    addition on panel color

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
-
+var $type="tee",$color="black",$y_pos="front",$nos_icons=0,$nos_text=0,$custom_img=0;
 $(function(){
+	$(".play-area").addClass($color);
 	$(".dr").draggable({
 		cursor: "move",
     	containment: [20,20,400,500],
 	});
 	$(".test").resizable();
+	$("#color-pick").click(function(){
+		$(".c-pick").toggleClass('hid');
+	});
+	$('#red').click(function(){
+				$(".play-area").removeClass($color);
+				$color="red";
+				$(".play-area").addClass($color);
+	});
+	$('#black').click(function(){
+				$(".play-area").removeClass($color);
+				$color="black";
+				$(".play-area").addClass($color);
+	});
+	$('#white').click(function(){
+				$(".play-area").removeClass($color);
+				$color="white";
+				$(".play-area").addClass($color);
+	});
+	$('#green').click(function(){
+				$(".play-area").removeClass($color);
+				$color="green";
+				$(".play-area").addClass($color);
+	});
+	$('#navy').click(function(){
+				$(".play-area").removeClass($color);
+				$color="navy";
+				$(".play-area").addClass($color);
+	});	
+
+	
+
+	
+
 });
 
 // $(function() { 
