08957cb20ea17f295ebb0715a744bfe77a32ca95
Keith3895 <keith30895@gmail.com>
2016-07-30 23:11:19 +0530

    added custom text
    custom text can be added to the play area.

--- js/main.js
+++ js/main.js
@@ -54,11 +54,49 @@ $(function(){
 		 		maxWidth:300,
 		 		minHeight:50,
 		 		minWidth:50,
-		 		// helper: "resizable-helper"
+
 			});
 		}
 	});
+	for(i=0;i<100;i++)
+	document.querySelector('#font_size').innerHTML+="<option value=\""+Number(i)+"\"> "+i+"px</option>";
 	
+	$('#apply_text').click(function(){
+		
+		var text_val = jQuery("textarea#custom_text").val();
+		if(!text_val)
+			return false;
+		
+			$(".play-area").append("<div id=text"+($nos_text)+" class='new_text' style=\"background:none;\"></span><textarea id='text_style' >"+text_val+"</textarea></div>");
+			$( "#text"+($nos_text)+"" ).draggable({
+				cursor: "move",
+		    	containment: [20,20,400,600],
+			});
+			$( "#text"+($nos_text)+"" ).resizable({
+				maxHeight: 480,
+				maxWidth: 450,
+				minHeight: 60,
+				minWidth: 60
+			});
+
+		var $font_ = $('#custom_text').css("font-family");
+		var $font_size= $('#custom_text').css("font-size");
+		var $font_weight= $('#custom_text').css("font-weight");
+		var $font_style	= $('#custom_text').css("font-style");
+		var $font_color = $('#custom_text').css("color");
+		//alert($font_u);
+		
+		
+		$("#text"+($nos_text)+" textarea" ).css("font-family", $font_);
+		$("#text"+($nos_text)+" textarea" ).css("font-size", $font_size);
+		$("#text"+($nos_text)+" textarea" ).css("font-weight", $font_weight);
+		$("#text"+($nos_text)+" textarea" ).css("font-style", $font_style);
+		$("#text"+($nos_text)+" textarea" ).css("color", $font_color);
+		$("#text"+($nos_text)).css({'top':'100px','left':'150px'});
+		$("#text"+($nos_text)+" textarea" ).css({'background':'none','width':'50px'});
+		//document.getElementById("text"+($nos_text)+" textarea").style.textDecoration=(""+$font_u+"");
+		++$nos_text;
+	});
 });
 
 // $(function() { 
---
+++ js/text-area.js
@@ -0,0 +1,43 @@
+function changeval() {
+		$total = parseInt($("#small").val()) + parseInt($("#medium").val()) + parseInt($("#large").val()) + parseInt($("#xlarge").val()) + parseInt($("#xxlarge").val());
+		//alert($total);
+		$('.small').val($("#small").val());
+		$('.medium').val($("#medium").val());
+		$('.large').val($("#large").val());
+		$('.xlarge').val($("#xlarge").val());
+		$('.xxlarge').val($("#xxlarge").val());
+		$('.total').val($total);
+	}
+
+	function changeval2() {
+		$total = parseInt($("#small2").val()) + parseInt($("#medium2").val()) + parseInt($("#large2").val()) + parseInt($("#xlarge2").val()) + parseInt($("#xxlarge2").val());
+		//alert($total);
+		$('.small').val($("#small2").val());
+		$('.medium').val($("#medium2").val());
+		$('.large').val($("#large2").val());
+		$('.xlarge').val($("#xlarge2").val());
+		$('.xxlarge').val($("#xxlarge2").val());
+		$('.total').val($total);
+	}
+
+	function b() {
+		$('#custom_text').toggleClass('bold_text');
+
+	}
+
+	function i() {
+		$('#custom_text').toggleClass('italic_text');
+	}
+
+	function changeFont(_name) {
+		$('#custom_text').css("font-family", _name);
+	}
+
+	function changeFontSize(_size) {
+		console.log(_size);
+		$('#custom_text').css("font-size", _size+"px");
+	}
+
+	function changeColor(_color) {
+		$('#custom_text').css("color", _color);
+	}
