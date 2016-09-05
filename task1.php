0d7915ba17bcdba18c16b37879cd2649404c90f7
Keith3895 <keith30895@gmail.com>
2016-09-05 20:22:46 +0530

    -color add

---
+++ task1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task1</title>
+	<link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+	<!-- <link rel="stylesheet" href="bootstrap.min.css"> -->
+	<link rel="stylesheet" href="styleList.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+<style type="text/css">
+	#project{
+		background: #e75c4d;
+	}
+	#profile{
+		background: #3f5060;
+	}
+</style>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-6" id="project">
+			<div>
+				<div class="text-right">
+					<span>
+						<p>Want to hire an Event Manager?</p>
+						<p>Post your project here</p>
+					</span>
+
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6" id="profile">
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
